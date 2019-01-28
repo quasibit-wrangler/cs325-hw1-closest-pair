0e0b4f14b032d877b516f79b09a8ba910529ad9f
logan miller <millelog@oregonstate.edu>
2019-01-28 12:40:55 -0800

    Added min_distance in partb and the assignment pdf

--- partb.py
+++ partb.py
@@ -2,15 +2,15 @@ import helperLibrary as helperfoos
 
 def naiveApproach(messyStuff):
     #sort by x for the first time
-    messyStuff.sort(key=lambda x: x[0])
+    messyStuff.sort(key=lambda point: point[0])
     sortByX = messyStuff
-    print(sortByX)
+    print("array sorted by x:\n", sortByX)
     results = split(sortByX)
 
     return results
 
 def scanEntireArray(array):
-
+    print("Y-sorted Array: \n", array)
     return
 
 # p1 and p2 are pairs of points or many pairs of points
@@ -31,7 +31,21 @@ def min_point(p1,p2):
     else:
         return p2
 
-
+def min_distance(p1, p2):
+    if(p1==None):
+        return p2
+    elif(p2==None):
+        return p1
+    minDistance1 = helperfoos.dist_between_points(p1[0], p1[1])
+    minDistance2 = helperfoos.dist_between_points(p2[0], p2[1])
+    if(minDistance1<minDistance2):
+        return p1
+    elif(minDistance1==minDistance2):
+        for item in p2:
+            p1.append(item)
+        return p1
+    else:
+        return p2
 
 
 def split(Array):
@@ -40,9 +54,11 @@ def split(Array):
     elif(len(Array)==2):
         return Array
     else:
-        closest1 = min_point(split(Array[:len(Array)/2]),split(Array[len(Array/2)+1:]))
-        Array.sort(key=lambda x: x[1]) #sort by the second element
-        closest0 = scanEntireArray(array)
+        midPoint = len(Array)/2
+        closest1 = min_distance(split(Array[:midPoint]),split(Array[midPoint+1:]))
+        Array.sort(key=lambda point: point[1]) #sort by the second element
+        print ("closest1: ", closest1)
+        closest0 = scanEntireArray(Array)
 
         return closest0 if(closest0<closest1) else closest1
 
@@ -52,7 +68,7 @@ def split(Array):
 def main():
     points = helperfoos.grabArray()
     results=naiveApproach(points)
-    helperfoos.printResults(results,helperfoos.dist_between_points(results[0][0],results[0][1]))
+    #helperfoos.printResults(results,helperfoos.dist_between_points(results[0][0],results[0][1]))
 
 
 
