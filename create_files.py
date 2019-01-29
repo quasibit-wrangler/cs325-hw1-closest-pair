729c5543024c7060f63bf622f16426034096b92f
sleek-turtle <reach.maddaloc@gmail.com>
2019-01-29 13:02:03 -0800

    added helper library function for creating input files

---
+++ create_files.py
@@ -0,0 +1,5 @@
+import helperLibrary as h
+
+
+
+h.createTestFile("./files/file1.txt",1000,50)
--- helperLibrary.py
+++ helperLibrary.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import random
 
 def grabArray():
     with open(sys.argv[1]) as f:
@@ -21,6 +22,8 @@ def dist_between_points(p1,p2):
     return math.sqrt(x**2 + y**2)
 
 def clean_duplicates(array):
+    ## haha recusive function with no base case
+    # i sure hope eventually we run out of duplicates!
     for i,pair1 in enumerate(array):
         for j in range(i+1,len(array)):
             if(  pair1[0][0] == array[j][0][0] and
@@ -30,10 +33,6 @@ def clean_duplicates(array):
                  del array[i]
                  array = clean_duplicates(array)
                  return array
-
-
-
-
     return array
 
 
@@ -44,4 +43,18 @@ def printResults(array,distance):
     for item in array:
         item.sort(key=lambda x: x[1])
         print("{x1} {y1} {x2} {y2}".format(x1= item[0][0], y1=item[0][1], x2=item[1][0], y2=item[1][1]))
+    return
+
+
+# creates data file with specified parameters
+
+#x and y values ranging from [-number_size_range, number_size_range]
+# number of coordinate pairs == number_pairs
+def createTestFile(file_name,number_size_range,number_pairs):
+    with open(file_name, 'w') as file:
+        for i in range ( number_pairs ):
+            int1 = random.randint(-1*number_size_range,1+number_size_range)
+            int2 = random.randint(-1*number_size_range,1+number_size_range)
+            file.write("{x} {y}\n".format(x=int1, y=int2))
+
     return
